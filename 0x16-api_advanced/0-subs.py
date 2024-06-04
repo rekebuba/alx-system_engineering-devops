3f2b655fbdf18790c4bacff2cdd64c73c698a039
rekebuba <abukiabdullahi1@gmail.com>
2024-06-04 15:01:37 +0300

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""return the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and
+    returns the number of subscribers
+    """
+    URL = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    try:
+        result = requests.get(URL)
+        if result.status_code != 200:
+            raise Exception
+        else:
+            result = result.json()
+    except Exception:
+        return 0
+
+    return result['data']['subscribers']
