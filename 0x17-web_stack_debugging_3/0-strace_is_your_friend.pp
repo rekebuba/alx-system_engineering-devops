99a7e552f0615d533d1252680d4715ecfacfe41b
rekebuba <abukiabdullahi1@gmail.com>
2024-06-05 23:07:48 +0300

    0x18-webstack_monitoring

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,10 @@
-# Define the path to the file
-$file_path = '/var/www/html/index.html'
-$content = 'Hello world'
+# A puppet manuscript to replace a line in a file on a server
 
-# Exec resource to create the file if it does not exist or is empty
-exec { 'create_file_if_empty_or_nonexistent':
-  command => "/bin/bash -c 'if [ ! -s ${file_path} ]; then echo \"${content}\" > ${file_path}; fi'",
-  path    => ['/bin', '/usr/bin'],
-  creates => $file_path,
-}
+$file_to_edit = '/var/www/html/wp-settings.php'
 
-# File resource to manage file properties
-file { $file_path:
-  ensure  => 'file',
-  mode    => '0644',
-  # This will ensure the file properties are correct, but will not overwrite content if the file already exists
-  require => Exec['create_file_if_empty_or_nonexistent'],
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
