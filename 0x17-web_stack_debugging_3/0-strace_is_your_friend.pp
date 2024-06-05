cb4c9610b8429a961eaf8cfa632fb4968358b154
rekebuba <abukiabdullahi1@gmail.com>
2024-06-05 22:23:10 +0300

    0x18-webstack_monitoring

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,18 @@
+# Define the path to the file
+$file_path = '/var/www/html/index.html'
+$content = 'Hello world'
+
+# Exec resource to create the file if it does not exist or is empty
+exec { 'create_file_if_empty_or_nonexistent':
+  command => "/bin/bash -c 'if [ ! -s ${file_path} ]; then echo \"${content}\" > ${file_path}; fi'",
+  path    => ['/bin', '/usr/bin'],
+  creates => $file_path,
+}
+
+# File resource to manage file properties
+file { $file_path:
+  ensure => 'file',
+  mode   => '0644',
+  # This will ensure the file properties are correct, but will not overwrite content if the file already exists
+  require => Exec['create_file_if_empty_or_nonexistent'],
+}
